89123071f7cd67150fb385742584f06c2f1e8d8e
Jingwen Wang <w.jingwen@yahoo.com>
2019-03-13 14:22:38 +1100

    :lipstick: add active link style

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem} from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
   
 export default class Header extends Component {
     constructor(props) {
       super(props);
-  
       this.toggle = this.toggle.bind(this);
       this.state = {
-        isOpen: false
+        isOpen: false,
+        activeLink: 'home'
       };
     }
 
@@ -23,25 +23,30 @@ export default class Header extends Component {
         <header>
           <Navbar color="dark" dark expand="md">
             <div className="container">
-            <Link className="navbar-brand" to="/home"><strong>Jing</strong></Link>
+            <NavLink className="navbar-brand" to="/home"><strong>Jing</strong></NavLink>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                    <Link className="nav-link" to='/home'>Home</Link>
+                    <NavLink className="nav-link" to='/home' activeClassName="active">Home</NavLink>
+                    <span></span>
                 </NavItem>
                 <NavItem>
-                    <Link className="nav-link" to='/about'>About</Link>
+                    <NavLink className="nav-link" to='/about' activeClassName="active">About</NavLink>
+                    <span></span>
                 </NavItem>
                 <NavItem>
-                    <Link className="nav-link" to='/skills'>Skills</Link>
+                    <NavLink className="nav-link" to='/skills' activeClassName="active">Skills</NavLink>
+                    <span></span>
                 </NavItem>
                 <NavItem>
-                    <Link className="nav-link" to='/projects'>Projects</Link>
+                    <NavLink className="nav-link" to='/projects' activeClassName="active">Projects</NavLink>
+                    <span></span>
                 </NavItem>
 
                 <NavItem>
-                    <Link className="nav-link" to='/home'>Contact</Link>
+                    <NavLink className="nav-link" to='/home' activeClassName="active">Contact</NavLink>
+                    <span></span>
                 </NavItem>
               </Nav>
             </Collapse>
