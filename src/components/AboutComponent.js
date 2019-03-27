9800a11ede230f193c94d023d8d0e73420fdf9e8
Jingwen Wang <w.jingwen@yahoo.com>
2019-03-27 17:02:41 +1100

    :speech_balloon: add more content

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
 
-export default class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isMount : false
+function RenderEducation() {
+    const educatinExp = [
+        {
+            course: "Full-Stack Web Development with React",
+            time: "Jan 2019 - Current",
+            provider: "The Hong Kong University of Science and Technology on Coursera"
+        },
+        {
+            course: "HTML, CSS, and Javascript for Web Developers",
+            time: "Dec 2018",
+            provider: "Johns Hopkins University on Coursera"
+        },
+        {
+            course: "Master Degree of Mechanical Engineer",
+            time: "Feb 2016 - Jul 2018",
+            provider: "University of Melbourne"
         }
-    }
+    ];
 
-    componentDidMount() {
-        this.setState({isMount: true})
-    }
+    return (
+        educatinExp.map((experience, index) => 
+            <li className="education-list-item" key={index}>
+                <p>{experience.course}<br/>
+                <span>{experience.time}</span></p>
+                <span>{experience.provider}</span>
+            </li>
+        )
+    );
+}
+
+export default class About extends Component {
 
     render() {
         return (
-            <section className={this.state.isMount? "about mounted" :"about"} id="about-section">
-                <div><h1><span className="h1-filler"></span>About Me</h1></div>
+            <section className="about" id="about-section">
+                <h1>About Me</h1>
                 <div className="container">
                     <div className="row d-flex align-items-stretch">
                         <div className="col-12 col-md-6 general-info-container">
                             <div className="text order-1 order-md-0">
                                 <p>Hi, nice to meet you. I am Jingwen Wang, a self-taught front-end web developer.</p>
-                                <p>I graduated from the University of Melbourne with a master degree of Mechanical Engineering, but I soon discovered my interest in web development when I was learning web scrapping using Python. </p>
-                                <p>Therefore, I invested my time on doing online courses, reading documentation and practicing what I learned by doing small projects. It is really exciting to build things from scratches.</p>
+                                <p>I graduated from the University of Melbourne with a master degree of Mechanical Engineering in 2018. I soon discovered my interest in web development when I was experimenting on web scrapping. </p>
+                                <p>Therefore, I invested my time on doing online courses and practicing what I learned by working on small projects. Now, I truly enjoy web development. It always gives me the same vibe of writing an essay using LaTex - it is elegant and fun!</p>
+                                <p>As an engineer, I have confident in my math ability and problem solving skills. As a self-taught front-end developer, I learn things fast and never give up easily. As someone enjoying their free time, I like things that is challenging, such as rock climbing and ARPG games.</p>
                             </div>
                             <div className="space-filler d-none d-md-block"></div>
                             <div className="title order-0 order-md-1">
-                                <h2>Who is Jing?</h2>
-                                <span>Jing</span>
+                                <h2>Profile</h2>
+                                <span>Profile</span>
                             </div>
                         </div>
                         <div className="col-12 col-md-6 education-info-container">
@@ -37,9 +58,9 @@ export default class About extends Component {
                             </div>
                             <div className="space-filler d-none d-md-block"></div>
                             <div className="text">
-                                <p>Hi, nice to meet you. I am Jingwen Wang, a self-taught front-end web developer.</p>
-                                <p>I graduated from the University of Melbourne with a master degree of Mechanical Engineering, but I soon discovered my interest in web development when I was learning web scrapping using Python. </p>
-                                <p>Therefore, I invested my time on doing online courses, reading documentation and practicing what I learned by doing small projects. It is really exciting to build things from scratches.</p>
+                                <ul>
+                                    <RenderEducation />
+                                </ul>
                             </div>
                         </div>
                     </div>
--- src/components/SkillsComponent.js
+++ src/components/SkillsComponent.js
@@ -33,7 +33,7 @@ export default class Skills extends Component {
                             <span className="fab fa-3x fa-js"></span>
                         </div>
                         <div className="col-12 skills-text">
-                            <p>I am confident of using <span className="keyword">JavaScript</span> to add functionality and interactivity to my websites. I pursuit writing clear, clean and effective codes and take browser compatibility into consideration. I am comfortable with writing <span className="keyword">React</span> application and manage states using <span className="keyword">Redux</span>. I also learned how to setup a simple <span className="keyword">Node.js</span> serve.</p>
+                            <p>I am confident of using <span className="keyword">JavaScript</span> to add functionality and interactivity to my websites. I pursuit writing clear, clean and reusable codes. I am comfortable with writing <span className="keyword">React</span> applications and managing states using <span className="keyword">Redux</span>. I also learned how to setup a simple <span className="keyword">Node.js</span> serve.</p>
                         </div>
                     </div>
                     <div className="col-12 col-md-6 col-lg-3 tools">
@@ -41,7 +41,7 @@ export default class Skills extends Component {
                             <span className="fas fa-3x fa-laptop"></span>
                         </div>
                         <div className="col-12 skills-text">
-                            <p>I am familiar with CSS preprocessor <span className="keyword">Sass</span> and I know how to configure tasks by <span className="keyword">Gulp</span>. I also have knowledge of using <span className="keyword">Git</span> and prefer writing clear and conventional commits. </p>
+                            <p>I am familiar with CSS preprocessor <span className="keyword">Sass</span> and I know how to configure tasks by <span className="keyword">Gulp</span>. I also have knowledge of using <span className="keyword">Git</span> and prefer writing clear commits. </p>
                         </div>
                     </div>
                 </div>
