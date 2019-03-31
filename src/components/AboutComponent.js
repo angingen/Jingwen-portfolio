7d698d3fd62de5b7b144fef426d9c4b74021cee8
Jingwen Wang <w.jingwen@yahoo.com>
2019-04-01 10:52:07 +1100

    :speech_balloon: modify profile content

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -42,7 +42,7 @@ export default class About extends Component {
                             <div className="text order-1 order-md-0"> 
                                 <p>Hi, nice to meet you. I am Jingwen Wang, a self-taught front-end web developer.</p>
                                 <p>I graduated from the University of Melbourne with a master degree of Mechanical Engineering in 2018. I soon discovered my interest in web development when I was experimenting on web scrapping. </p>
-                                <p>Therefore, I invested my time on doing online courses and practicing what I learned by working on small projects. Now, I truly enjoy web development. It always gives me the same vibe of writing an essay using LaTex - it is elegant and fun!</p>
+                                <p>Therefore, I invested my time on doing online courses and practicing what I learned by working on small projects. Now, I truly enjoy web development.</p>
                                 <p>As an engineer, I have confident in my math ability and problem solving skills. As a self-taught front-end developer, I learn things fast and never give up easily. As someone enjoying their free time, I like things that is challenging, such as rock climbing and ARPG games.</p>
                             </div>
                             <div className="space-filler d-none d-md-block"></div>
